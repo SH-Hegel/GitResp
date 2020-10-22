c5ace76d7249c620006d3605218d6ed59f53dec2
hegel <sh_zwx@126.com>
2020-10-22 20:25:53 +0800

    lucene import

---
+++ src/lib/FirstLucene.java
@@ -0,0 +1,22 @@
+package lib;
+
+import org.junit.Test;
+
+public class FirstLucene {
+
+	@Test
+	public void testBuildIndex() throws Exception {
+		
+		//嘎嘎嘎
+		
+		
+		
+		
+		
+	}
+	
+	@Test
+	public void testFunc() {
+		
+	}
+}
